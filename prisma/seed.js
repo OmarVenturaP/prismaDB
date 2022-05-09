7ea34219fde075df47571fad99f73045736aa1c7
Omar Ventura <99059639+OmarVenturaP@users.noreply.github.com>
2022-05-09 04:44:43 -0500

    Paso 28) Agregando ESLinter para mejorar legibilidad del código

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,176 +1,176 @@
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 (async function main() {
-  try {
-    const woopa = await prisma.explorer.upsert({
-        where: { name: 'Woopa' },
-        update: {},
-        create: {
-            name: 'Woopa',
-			username: 'ajolonauta',
-			mission: 'Node'
-      },
-    });
+    try {
+        const woopa = await prisma.explorer.upsert({
+            where: { name: "Woopa" },
+            update: {},
+            create: {
+                name: "Woopa",
+                username: "ajolonauta",
+                mission: "Node"
+            },
+        });
 
-    const woopa1 = await prisma.explorer.upsert({
-        where: { name: 'Woopa1' },
-        update: {},
-        create: {
-        name: 'Woopa1',
-			username: 'ajolonauta1',
-			mission: 'Node'
-      },
-    });
+        const woopa1 = await prisma.explorer.upsert({
+            where: { name: "Woopa1" },
+            update: {},
+            create: {
+                name: "Woopa1",
+                username: "ajolonauta1",
+                mission: "Node"
+            },
+        });
 
-    const woopa2 = await prisma.explorer.upsert({
-        where: { name: 'Woopa 2' },
-        update: {},
-        create: {
-            name: 'Woopa 2',
-			username: 'ajolonauta2',
-			mission: 'Java'
-      },
-    });
+        const woopa2 = await prisma.explorer.upsert({
+            where: { name: "Woopa 2" },
+            update: {},
+            create: {
+                name: "Woopa 2",
+                username: "ajolonauta2",
+                mission: "Java"
+            },
+        });
 
-    const woopa3 = await prisma.explorer.upsert({
-        where: { name: 'Woopa 3' },
-        update: {},
-        create: {
-            name: 'Woopa 3',
-			username: 'ajolonauta3',
-			mission: 'Node'
-      },
-    });
-    const woopa4 = await prisma.explorer.upsert({
-        where: { name: 'Woopa 4' },
-        update: {},
-        create: {
-            name: 'Woopa 4',
-            username: 'ajolonauta4',
-            mission: 'Node'
-        },
-    });
-    const woopa5 = await prisma.explorer.upsert({
-        where: { name: 'Woopa 5' },
-        update: {},
-        create: {
-            name: 'Woopa 5',
-            username: 'ajolonauta5',
-            mission: 'Node'
-        },
-    });
-    const woopa6 = await prisma.explorer.upsert({
-        where: { name: 'Woopa 6' },
-        update: {},
-        create: {
-            name: 'Woopa 6',
-            username: 'ajolonauta6',
-            mission: 'Node'
-        }
-    });
-    const woopa7 = await prisma.explorer.upsert({
-        where: { name: 'Woopa 7' },
-        update: {},
-        create: {
-            name: 'Woopa 7',
-            username: 'ajolonauta7',
-            mission: 'Node'
-        }
-    });
-    const woopa8 = await prisma.explorer.upsert({
-        where: { name: 'Woopa 8' },
-        update: {},
-        create: {
-            name: 'Woopa 8',
-            username: 'ajolonauta8',
-            mission: 'Node'
-        }
-    });
-    const woopa9 = await prisma.explorer.upsert({
-        where: { name: 'Woopa 9' },
-        update: {},
-        create: {
-            name: 'Woopa 9',
-            username: 'ajolonauta9',
-            mission: 'Node'
-        }
-    });
+        const woopa3 = await prisma.explorer.upsert({
+            where: { name: "Woopa 3" },
+            update: {},
+            create: {
+                name: "Woopa 3",
+                username: "ajolonauta3",
+                mission: "Node"
+            },
+        });
+        const woopa4 = await prisma.explorer.upsert({
+            where: { name: "Woopa 4" },
+            update: {},
+            create: {
+                name: "Woopa 4",
+                username: "ajolonauta4",
+                mission: "Node"
+            },
+        });
+        const woopa5 = await prisma.explorer.upsert({
+            where: { name: "Woopa 5" },
+            update: {},
+            create: {
+                name: "Woopa 5",
+                username: "ajolonauta5",
+                mission: "Node"
+            },
+        });
+        const woopa6 = await prisma.explorer.upsert({
+            where: { name: "Woopa 6" },
+            update: {},
+            create: {
+                name: "Woopa 6",
+                username: "ajolonauta6",
+                mission: "Node"
+            }
+        });
+        const woopa7 = await prisma.explorer.upsert({
+            where: { name: "Woopa 7" },
+            update: {},
+            create: {
+                name: "Woopa 7",
+                username: "ajolonauta7",
+                mission: "Node"
+            }
+        });
+        const woopa8 = await prisma.explorer.upsert({
+            where: { name: "Woopa 8" },
+            update: {},
+            create: {
+                name: "Woopa 8",
+                username: "ajolonauta8",
+                mission: "Node"
+            }
+        });
+        const woopa9 = await prisma.explorer.upsert({
+            where: { name: "Woopa 9" },
+            update: {},
+            create: {
+                name: "Woopa 9",
+                username: "ajolonauta9",
+                mission: "Node"
+            }
+        });
 
-    const Warren = await prisma.student.upsert({
-        where: { name: 'Warren' },
-        update: {},
-        create: {
-            name: 'Warren',
+        const Warren = await prisma.student.upsert({
+            where: { name: "Warren" },
+            update: {},
+            create: {
+                name: "Warren",
         	lang: "elixir",
-            missionComander: "Carlo Gilmar",
-            enrollments:678,
-            hasCertification: true
-        },
-    });
+                missionComander: "Carlo Gilmar",
+                enrollments:678,
+                hasCertification: true
+            },
+        });
 
-    const Lucinda = await prisma.student.upsert({
-        where: { name: 'Lucinda' },
-        update: {},
-        create: {
-            name: 'Lucinda',
+        const Lucinda = await prisma.student.upsert({
+            where: { name: "Lucinda" },
+            update: {},
+            create: {
+                name: "Lucinda",
         	lang: "node",
-            missionComander: "Carlo Gilmar",
-            enrollments: 546,
-            hasCertification: true
-        },
-    });
+                missionComander: "Carlo Gilmar",
+                enrollments: 546,
+                hasCertification: true
+            },
+        });
 
-    const Fuentes = await prisma.student.upsert({
-        where: { name: 'Fuentes' },
-        update: {},
-        create: {
-            name: 'Fuentes',
-            lang: "java",
-            missionComander: "Carlo Gilmar",
-            enrollments:  342,
-            hasCertification: true
-        },
-    });
-    const Joseph = await prisma.fellow.upsert({
-        where: { name: 'Joseph' },
-        update: {},
-        create: {
-            name: 'Joseph',
-            username: "venturas99",
-            mainStack: "NodeJS",
-            currentEnrollmend:  true,
-            hasAzureCertification: true
-        },
-    });
-    const Raul = await prisma.fellow.upsert({
-        where: { name: 'Raul' },
-        update: {},
-        create: {
-            name: 'Raul',
-            username: "veles",
-            mainStack: "java",
-            currentEnrollmend:  true,
-            hasAzureCertification: false
-        },
-    });
-    const Grettel = await prisma.fellow.upsert({
-        where: { name: 'Grettel' },
-        update: {},
-        create: {
-            name: 'Grettel',
-            username: "villareal",
-            mainStack: "PHP",
-            currentEnrollmend:  false,
-            hasAzureCertification: true
-        },
-    });
+        const Fuentes = await prisma.student.upsert({
+            where: { name: "Fuentes" },
+            update: {},
+            create: {
+                name: "Fuentes",
+                lang: "java",
+                missionComander: "Carlo Gilmar",
+                enrollments:  342,
+                hasCertification: true
+            },
+        });
+        const Joseph = await prisma.fellow.upsert({
+            where: { name: "Joseph" },
+            update: {},
+            create: {
+                name: "Joseph",
+                username: "venturas99",
+                mainStack: "NodeJS",
+                currentEnrollmend:  true,
+                hasAzureCertification: true
+            },
+        });
+        const Raul = await prisma.fellow.upsert({
+            where: { name: "Raul" },
+            update: {},
+            create: {
+                name: "Raul",
+                username: "veles",
+                mainStack: "java",
+                currentEnrollmend:  true,
+                hasAzureCertification: false
+            },
+        });
+        const Grettel = await prisma.fellow.upsert({
+            where: { name: "Grettel" },
+            update: {},
+            create: {
+                name: "Grettel",
+                username: "villareal",
+                mainStack: "PHP",
+                currentEnrollmend:  false,
+                hasAzureCertification: true
+            },
+        });
 
-    console.log('Create 3 fellows');
-  } catch(e) {
-    console.error(e);
-    process.exit(1);
-  } finally {
-    await prisma.$disconnect();
-  }
+        console.log("Create 3 fellows");
+    } catch(e) {
+        console.error(e);
+        process.exit(1);
+    } finally {
+        await prisma.$disconnect();
+    }
 })();
--- prisma/server.js
+++ prisma/server.js
@@ -1,160 +1,160 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 app.use(express.json());
 const port = process.env.PORT || 3000;
 
-const { PrismaClient } = require('@prisma/client')
-const prisma = new PrismaClient()
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
-app.get('/', (request, response) => {
-    response.json({message: 'alive'});
+app.get("/", (request, response) => {
+    response.json({message: "alive"});
 });
 
-app.get('/explorers', async (request, response) => {
-    const allExplorers = await prisma.explorer.findMany({})
+app.get("/explorers", async (request, response) => {
+    const allExplorers = await prisma.explorer.findMany({});
     response.set("Access-Control-Allow-Origin", "*");
-    response.json(allExplorers)
+    response.json(allExplorers);
 });
 
-app.get('/explorers/:id', async (request, response) => {
-    const id = request.params.id
+app.get("/explorers/:id", async (request, response) => {
+    const id = request.params.id;
     const explorer = await prisma.explorer.findUnique({where: {id: parseInt(id)}});
     response.set("Access-Control-Allow-Origin", "*");
     response.json(explorer);
 });
 
-app.post('/explorers', async (request, response) => {
+app.post("/explorers", async (request, response) => {
     const explorer = {
-      name: request.body.name,
-      username: request.body.username,
-      mission: request.body.mission
-     };
-    const message = 'Explorer creado.';
+        name: request.body.name,
+        username: request.body.username,
+        mission: request.body.mission
+    };
+    const message = "Explorer creado.";
     await prisma.explorer.create({data: explorer});
     response.set("Access-Control-Allow-Origin", "*");
     return response.json({message});
-  });
-
-  app.put('/explorers/:id', async (req, res) => {
-	const id = parseInt(req.params.id);
-	await prisma.explorer.update({
-		where: {
-			id: id
-		},
-		data: {
-			mission: req.body.mission
-		}
-	})
-    res.set("Access-Control-Allow-Origin", "*");
-	return res.json({message: "Actualizado correctamente"});
 });
 
-app.delete('/explorers/:id', async (request, response) => {
-	const id = parseInt(request.params.id);
-	await prisma.explorer.delete({where: {id: id}});
+app.put("/explorers/:id", async (req, res) => {
+    const id = parseInt(req.params.id);
+    await prisma.explorer.update({
+        where: {
+            id: id
+        },
+        data: {
+            mission: req.body.mission
+        }
+    });
+    res.set("Access-Control-Allow-Origin", "*");
+    return res.json({message: "Actualizado correctamente"});
+});
+
+app.delete("/explorers/:id", async (request, response) => {
+    const id = parseInt(request.params.id);
+    await prisma.explorer.delete({where: {id: id}});
     response.set("Access-Control-Allow-Origin", "*");
-	return response.json({message: "Eliminado correctamente"});
+    return response.json({message: "Eliminado correctamente"});
 });
 
 // Modelo Students
-app.get('/students', async (request, response) => {
-    const allStudents = await prisma.student.findMany({})
+app.get("/students", async (request, response) => {
+    const allStudents = await prisma.student.findMany({});
     response.set("Access-Control-Allow-Origin", "*");
-    response.json(allStudents)
+    response.json(allStudents);
 });
 
-app.get('/students/:id', async (request, response) => {
-    const id = request.params.id
+app.get("/students/:id", async (request, response) => {
+    const id = request.params.id;
     const student = await prisma.student.findUnique({where: {id: parseInt(id)}});
     response.set("Access-Control-Allow-Origin", "*");
     response.json(student);
 });
 
-app.post('/students', async (request, response) => {
+app.post("/students", async (request, response) => {
     const student = {
         name: request.body.name,
         lang: request.body.lang,
         missionComander: request.body.missionComander,
         enrollments: request.body.enrollments,
         hasCertification: request.body.hasCertification
-     };
-    const message = 'Student creado.';
+    };
+    const message = "Student creado.";
     await prisma.student.create({data: student});
     response.set("Access-Control-Allow-Origin", "*");
     return response.json({message});
 });
 
-app.put('/students/:id', async (request, response) => {
-	const id = parseInt(req.params.id);
-	await prisma.student.update({
-		where: {
-			id: id
-		},
-		data: {
+app.put("/students/:id", async (request, response) => {
+    const id = parseInt(req.params.id);
+    await prisma.student.update({
+        where: {
+            id: id
+        },
+        data: {
             lang: request.body.lang,
             missionComander: request.body.missionComander,
-		}
-	});
+        }
+    });
     return response.json({message: "Actualizado correctamente"});
 });
 
-app.delete('/students/:id', async (request, response) => {
-	const id = parseInt(request.params.id);
-	await prisma.student.delete({where: {id: id}});
+app.delete("/students/:id", async (request, response) => {
+    const id = parseInt(request.params.id);
+    await prisma.student.delete({where: {id: id}});
     response.set("Access-Control-Allow-Origin", "*");
-	return response.json({message: "Eliminado correctamente"});
+    return response.json({message: "Eliminado correctamente"});
 });
 
 // Modelo Fellows
-app.get('/fellows', async (request, response) => {
-    const allFellow = await prisma.fellow.findMany({})
+app.get("/fellows", async (request, response) => {
+    const allFellow = await prisma.fellow.findMany({});
     response.set("Access-Control-Allow-Origin", "*");
-    response.json(allFellow)
+    response.json(allFellow);
 });
 
-app.get('/fellows/:id', async (request, response) => {
-    const id = request.params.id
+app.get("/fellows/:id", async (request, response) => {
+    const id = request.params.id;
     const fellows = await prisma.fellow.findUnique({where: {id: parseInt(id)}});
     response.set("Access-Control-Allow-Origin", "*");
     response.json(fellows);
 });
 
-app.post('/fellows', async (request, response) => {
+app.post("/fellows", async (request, response) => {
     const fellow = {
         name: request.body.name,
         username: request.body.username,
         mainStack: request.body.mainStack,
         currentEnrollmend: request.body.currentEnrollmend,
         hasAzureCertification: request.body.hasAzureCertification
-     };
-    const message = 'fellow creado.';
+    };
+    const message = "fellow creado.";
     await prisma.fellow.create({data: fellow});
     response.set("Access-Control-Allow-Origin", "*");
     return response.json({message});
 });
 
-app.put('/fellows/:id', async (request, response) => {
-	const id = parseInt(req.params.id);
-	await prisma.fellow.update({
-		where: {
-			id: id
-		},
-		data: {
+app.put("/fellows/:id", async (request, response) => {
+    const id = parseInt(req.params.id);
+    await prisma.fellow.update({
+        where: {
+            id: id
+        },
+        data: {
             mainStack: request.body.mainStack,
-		}
-	});
+        }
+    });
     return response.json({message: "Actualizado correctamente"});
 });
 
-app.delete('/fellows/:id', async (request, response) => {
-	const id = parseInt(request.params.id);
-	await prisma.fellow.delete({where: {id: id}});
+app.delete("/fellows/:id", async (request, response) => {
+    const id = parseInt(request.params.id);
+    await prisma.fellow.delete({where: {id: id}});
     response.set("Access-Control-Allow-Origin", "*");
-	return response.json({message: "Eliminado correctamente"});
+    return response.json({message: "Eliminado correctamente"});
 });
 
 // Cors
-const cors = require('cors');
+const cors = require("cors");
 
 const corsOptions = {
     origin: "http://localhost:8081",
